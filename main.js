aea57e2ad98e12ae1ce698e250f128e941084eaa
Dorenalto Couto <dorenalto.couto@foursys.com.br>
2020-08-17 20:41:54 -0300

    Add hikes route and template.

--- main.js
+++ main.js
@@ -4,19 +4,21 @@ const store = require('./store/datastore');
 const initialStoreData = require('./store/data');
 const Musician = require('./models/musician');
 const musicianRoutes = require('./routes/musician');
-
+const hike = require('./routes/hike')
 const app = express();
 const port = process.env.PORT || 3001;
 
 // include routes
 app.use('/musician', musicianRoutes);
 
-app.use(express.static('public'));
+// app.use(express.static('public'));
 
 // Index route
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
+app.get('/hikes', hike.index);
+app.post('/add_hike', hike.add_hike);
 
 // initialize store
 const musician = new Musician(store);
