88d11b7e56460618dca3e9938f088dabebdea6a3
Dorenalto Couto <dorenalto.couto@foursys.com.br>
2020-08-17 23:04:21 -0300

    alteracao parpara elasticbeans

---
+++ README.md
@@ -0,0 +1,11 @@
+# musician-app
+NodeJS / React sample app for AWS CI/CD pipeline and AWS with DB
+
+
+#
+# Professor José Castillo
+
+# Aluno Dorenalto M Couto
+# Matricula 338609
+
+# awsCloud
---
+++ store/data.js
@@ -0,0 +1,38 @@
+// A collection of musicians where the key is the unique id
+module.exports = {
+  coltrane: {
+    firstName: 'Elvis',
+    lastName: 'Presley',
+    genre: 'Rock',
+  },
+
+  mars: {
+    firstName: 'Bruno',
+    lastName: 'Mars',
+    genre: 'POP',
+  },
+
+  mccartney: {
+    firstName: 'Paul',
+    lastName: 'McCartney',
+    genre: 'ROCK',
+  },
+
+  hendrix: {
+    firstName: 'Jimi',
+    lastName: 'Hendrix',
+    genre: 'Rock',
+  },
+
+  russo: {
+    firstName: 'Renato',
+    lastName: 'Russo',
+    genre: 'Rock',
+  },
+
+  pericles: {
+    firstName: 'Pericles',
+    lastName: 'Faria',
+    genre: 'Samba',
+  },
+};
